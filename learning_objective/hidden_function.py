384da4e20c89d53f506b2c1c30c12f6c3cef2617
Rui Shu <00ruishu@gmail.com>
2015-04-17 11:45:26 -0400

    Proper scaling and multivar compatible

--- learning_objective/hidden_function.py
+++ learning_objective/hidden_function.py
@@ -1,17 +1,21 @@
 import numpy as np
 import time
 
-def evaluate(query, scale):
+def evaluate(query, lim_domain):
+    """ Query is always a single query
+    """
     noiseless_g  = lambda x: 10*np.sin(x) - x
     g            = lambda x: noiseless_g(x) + np.random.randn()/10 # Define the hidden function
 
-    
-    dataset_X = np.atleast_2d(query)
-    dataset_Y = np.asarray([[g(scale*dataset_X[0, :])[0]]])
-    dataset = np.concatenate((dataset_X, dataset_Y), axis=1)
-
-    time.sleep(0.5)
-
+    # Transform to cube: scaled = (val - mean)/var. 
+    # Transform back: *scaled*var) + mean
+    var = (lim_domain[1, :] - lim_domain[0, :])/2.
+    mean = (lim_domain[1, :] + lim_domain[0, :])/2.
+    query = np.atleast_2d(query)
+    dataset_X = query*var + mean
+    dataset_Y = np.atleast_2d(g(dataset_X)[0, 0])
+    dataset = np.concatenate((query, dataset_Y), axis=1)
+    # time.sleep(0.5)
     return dataset
     
 def true_evaluate(query, scale):
